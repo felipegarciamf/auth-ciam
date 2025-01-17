f574757acae95878294c75853973f41ac554973e
Silas Felipe Garcia <Silas@MacBook-Air-de-Mac.local>
2025-01-17 09:10:27 -0300

    iniciando projeto

---
+++ src/test/java/br/com/servico/de/autenticacao/ServicoDeAutenticacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.servico.de.autenticacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicoDeAutenticacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
